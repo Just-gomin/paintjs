b2f9a81a4ae1bae3b83af1ba86b797f1dc5de1df
An KangMin <hkkm612@gmail.com>
2020-07-29 16:30:16 +0900

    Draw Line.

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
 
 let isPainting = false;
 
@@ -6,22 +13,29 @@ const stopPainting = () => {
   isPainting = false;
 };
 
+const startPainting = () => {
+  isPainting = true;
+};
+
 const onMouseMove = (event) => {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!isPainting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 };
 
 const onMouseDown = (event) => {
   isPainting = true;
 };
 
-const onMouseUp = (event) => {
-  stopPainting();
-};
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
