0a446734fdacd9ea442c2e97c61c401792c2439c
An KangMin <hkkm612@gmail.com>
2020-07-29 19:06:45 +0900

    Brush Size.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+const brushSizeInput = document.getElementById("jsBrushSize");
+const brushSizeText = document.getElementById("jsBrushSizeText");
+
+const mode = document.getElementById("jsMode");
+
 const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
@@ -10,6 +15,7 @@ ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
 
 let isPainting = false;
+let isFilling = false;
 
 const stopPainting = () => {
   isPainting = false;
@@ -35,6 +41,24 @@ const handleColorClick = (event) => {
   ctx.strokeStyle = event.target.style.backgroundColor;
 };
 
+const handleBrushSizeChange = (event) => {
+  const brushSize = event.target.value;
+  ctx.lineWidth = brushSize;
+  brushSizeText.innerText = brushSize.includes(".")
+    ? brushSize
+    : `${brushSize}.0`;
+};
+
+const handleModeClick = () => {
+  if (isFilling) {
+    isFilling = false;
+    mode.innerText = "FILL";
+  } else {
+    isFilling = true;
+    mode.innerText = "Paint";
+  }
+};
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -45,3 +69,11 @@ if (canvas) {
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (brushSizeInput) {
+  brushSizeInput.addEventListener("input", handleBrushSizeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
