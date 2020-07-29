dc4d446986be8d8530c3145069c7e52b7e684a2d
An KangMin <hkkm612@gmail.com>
2020-07-29 16:58:54 +0900

    Change Color.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+const colors = document.getElementsByClassName("jsColor");
+
 canvas.width = 700;
 canvas.height = 700;
 
@@ -29,13 +31,17 @@ const onMouseMove = (event) => {
   }
 };
 
-const onMouseDown = (event) => {
-  isPainting = true;
+const handleColorClick = (event) => {
+  ctx.strokeStyle = event.target.style.backgroundColor;
 };
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
